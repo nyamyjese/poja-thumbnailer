5667a503983cc83783891c8bdf10e2791476cc8c
nyamyjese <hei.jese.2@gmail.com>
2026-09-16 09:12:19 +0100

    feat(mapper): add submission mapper

---
+++ src/main/java/com/example/demo/mapper/SubmissionMapper.java
@@ -0,0 +1,18 @@
+package com.example.demo.mapper;
+
+import com.example.demo.dto.Submission;
+import com.example.demo.entity.SubmissionEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubmissionMapper {
+
+    public Submission toRest(SubmissionEntity entity) {
+        return Submission.builder()
+                .id(entity.getId())
+                .email(entity.getEmail())
+                .thumbnailKey(entity.getThumbnailKey())
+                .createdAt(entity.getCreatedAt())
+                .build();
+    }
+}
