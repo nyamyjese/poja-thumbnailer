ce04493592bf6684c59a55439bb3745df62649cb
nyamyjese <hei.jese.2@gmail.com>
2026-09-16 09:20:21 +0100

    feat(dto): add thumbnal requested

---
+++ src/main/java/com/example/demo/dto/ThumbnailRequested.java
@@ -0,0 +1,34 @@
+package com.example.demo.dto;
+
+import java.time.Duration;
+import java.util.UUID;
+
+import com.example.demo.endpoint.event.model.PojaEvent;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class ThumbnailRequested extends PojaEvent {
+    private UUID submissionId;
+    private String originalKey;
+    private String email;
+
+    @Override
+    public Duration maxConsumerDuration() {
+        return Duration.ofSeconds(60);
+    }
+
+    @Override
+    public Duration maxConsumerBackoffBetweenRetries() {
+        return Duration.ofSeconds(30);
+    }
+}
